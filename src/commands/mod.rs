edf485eb1b8f7fb59ecdf678430c52b33738ece4
MainSilent <hamidreza.neo@gmail.com>
2021-11-02 22:58:35 +0330

    add commands module

---
+++ src/commands/mod.rs
@@ -0,0 +1,2 @@
+mod push;
+pub use push::push;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
+mod commands;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1][..];
-    
+
     match command {
+        "push" => commands::push(),
         _ => {
             eprintln!("Please pass an argument")
         }
