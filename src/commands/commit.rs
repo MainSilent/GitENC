b15dc2367b65858a3d3f38a8ad7e9016ad2f85a5
MainSilent <hamidreza.neo@gmail.com>
2021-11-03 10:32:27 +0330

    Update commit.rs

--- src/commands/commit.rs
+++ src/commands/commit.rs
@@ -5,7 +5,13 @@ use flate2::Compression;
 use flate2::write::GzEncoder;
 
 pub fn commit() {
+    println!("Compression...");
     compress().unwrap();
+
+    println!("Encrypting...");
+    encrypt().unwrap();
+
+    println!("Done.");
 }
 
 fn compress() -> Result<(), std::io::Error> {
@@ -17,6 +23,10 @@ fn compress() -> Result<(), std::io::Error> {
     let tar_gz = File::create(filename)?;
     let enc = GzEncoder::new(tar_gz, Compression::default());
     let mut tar = Builder::new(enc);
-    tar.append_dir_all("bardir", ".").unwrap();
+    tar.append_dir_all("enc", ".").unwrap();
+    Ok(())
+}
+
+fn encrypt() -> Result<(), std::io::Error> {
     Ok(())
 }
