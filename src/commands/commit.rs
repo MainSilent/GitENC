b68edac91668c2ee8eb6146e14ae212168c52859
MainSilent <hamidreza.neo@gmail.com>
2021-11-03 10:24:44 +0330

    add compression

--- src/commands/commit.rs
+++ src/commands/commit.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use tar::Builder;
+use std::fs::File;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+
 pub fn commit() {
-    
+    compress().unwrap();
+}
+
+fn compress() -> Result<(), std::io::Error> {
+    let filename = "enc.tar.gz";
+
+    // remove last compression
+    fs::remove_file(filename).unwrap_or(());
+
+    let tar_gz = File::create(filename)?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = Builder::new(enc);
+    tar.append_dir_all("bardir", ".").unwrap();
+    Ok(())
 }
