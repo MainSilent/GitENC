6357b78a7e16a91127fcc7a2500a6bf0b5b6069f
MainSilent <hamidreza.neo@gmail.com>
2021-11-03 16:34:27 +0330

    Update clone.rs

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -1,3 +1,25 @@
+use std::io;
+use std::process::{Command, Stdio};
+
 pub fn clone(url: &str) {
-    println!("{}", url)
+    println!("Initializing git...");
+    init(url);
+}
+
+fn init(url: &str) {
+    let mut branch = String::new();
+    print!("\nPlease enter a branch name: ");
+    io::Write::flush(&mut io::stdout()).expect("flush failed!");
+    io::stdin().read_line(&mut branch).unwrap();
+
+    Command::new("git")
+        .args(["init", "."]).output().unwrap();
+
+    Command::new("git")
+        .args(["remote", "add", "origin", url]).output().unwrap();
+    
+    Command::new("git")
+        .args(["pull", "origin", "main"])
+        .stdout(Stdio::inherit())
+        .output().unwrap();
 }
